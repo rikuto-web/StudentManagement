aba9d759cec0c4e02a05ea5b89789820eeb067aa
Shinya <rikutoweb5@gmail.com>
2025-04-20 00:46:00 +0900

    講座17追加

--- src/main/java/student/management/StudentManagement/StudentManagementApplication.java
+++ src/main/java/student/management/StudentManagement/StudentManagementApplication.java
@@ -1,5 +1,6 @@
 package student.management.StudentManagement;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ public class StudentManagementApplication {
 	}
 
 	@GetMapping("/hello")
-	public String kon(){
+	public String hrllo(){
 		return "Hello,World";
 	}
 }
