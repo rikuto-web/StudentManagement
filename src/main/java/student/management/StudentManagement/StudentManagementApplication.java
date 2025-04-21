122543252bc22484bc01369c0d0e5f406bb3b1da
Shinya <rikutoweb5@gmail.com>
2025-04-21 09:37:14 +0900

    講座18課題作成

--- src/main/java/student/management/StudentManagement/StudentManagementApplication.java
+++ src/main/java/student/management/StudentManagement/StudentManagementApplication.java
@@ -3,36 +3,62 @@
 
 package student.management.StudentManagement;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-    private String name = "Tanabe Shinya";
-    private String age = "33";
+    private String name;
+    private String age;
+    private Map<String, String> personalData = new HashMap<>();
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
+
     }
 
-    @GetMapping("/studentInfo")
+    //入力フォーム
+    @GetMapping("/userInput")
+    public String userInput() {
+        return "氏名:" + name + " " + "年齢:" + age + "歳";
+    }
+
+    //入力の受け取りとMapへの追加処理
+    @PostMapping("/userInput")
+    public void userDate(String name, String age) {
+        this.name = name;
+        this.age = age;
+        personalData.put(name, age);
+    }
+    //一覧表示
+    @GetMapping("/userDateMap")
+    @ResponseBody
+    public Map<String, String> userMap() {
+        return personalData;
+    }
+
+
+    //下記講座内容
+    /*@GetMapping("/studentInfo")
     public String getStudentInfo() {
         return name + " " + age + "歳";
     }
     //↓これでもOK
 	/*@GetMapping("/age")
-	public String getAge(){
+	public String getAge("/){
 		return age;
 	}*/
 
-    @PostMapping("/studentInfo")
+    /*@PostMapping("/studentInfo")
     //複数の引数の場合　＆で渡せる
     public void setStudentInfo(String name, String age) {
         this.name = name;
